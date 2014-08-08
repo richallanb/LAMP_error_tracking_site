615f0cc7ba49767e1c8cd45234f86908b93422ca
TeamNine <teamnine@teamnine.com>
2014-08-08 04:51:01 +0000

    Commit with special folders for settings

---
+++ www/errors/error404.php
@@ -0,0 +1,58 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+ 
+    <title>404!?</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../team/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    
+    <style>
+    </style>
+  
+  </head>
+
+  <body>
+    
+    <script>
+    </script>
+    
+    <div class="container">
+      <div class="masthead" style="margin-top:20px;">
+     
+        <!-- Header-->
+        <h2 class="text-muted" style="display:table-cell;width:100%;">Team Nine</h2>
+        <h6 style="display:table-cell;min-width:300px;text-align:right;">
+          <?php
+            if(isset($_SESSION['logged'])){
+              print ('Welcome ' . '<a href="./php/logout.php">' . $_SESSION["user"] . '</a>');            
+            }else{
+              print ('<a href="./index.php">Sign in</a>');
+            }
+          ?>
+        </h6>
+      </div> <!-- /masthead -->
+
+      <!-- Jumbotron -->
+      <div id="landing" class="jumbotron">
+        <h1>
+          Four Hundred and Four
+        </h1>
+        <p class="lead">
+          You've reached this page as an error or extreme stupidity. You pick. I suggest you go 
+          <a href="../">back</a> 
+          and rethink your choices.
+        </p>       
+      </div>
+         
+      <!-- Site footer -->
+      <div class="footer">
+        <p>&copy; Nine 2014</p>
+      </div>
+
+    </div> <!-- /container -->
+  </body>
+</html>
