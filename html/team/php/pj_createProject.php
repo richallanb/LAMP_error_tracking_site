58705a959df6b6c3527216120d590e771e4b3c6e
TeamNine <teamnine@teamnine.com>
2014-09-05 22:52:50 +0000

    Hotfixes

--- html/team/php/pj_createProject.php
+++ html/team/php/pj_createProject.php
@@ -37,7 +37,7 @@
       
       // connection variable should've been defined by pj_validator
       if($connection->createProject($project, $caller, $caller_id) != 0){
-        // Error happened
+        echo $caller_id;
         header('HTTP/1.1 400 Bad Request');
         exit;
         
