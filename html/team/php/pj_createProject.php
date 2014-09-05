174dd7bdf9dce276f967e5c760569fa91d98d837
TeamNine <teamnine@teamnine.com>
2014-09-05 21:49:57 +0000

    Hotfixes

--- html/team/php/pj_createProject.php
+++ html/team/php/pj_createProject.php
@@ -28,7 +28,7 @@
   function action($session_id, $connection){
     
     // CPF - Create Project Form
-    $project = filter_var($_POST['CPFprojectname'], FILTER_SANITIZE_STRING);
+    $project = preg_filter('/((?![\w ]).)+/','',filter_var($_POST['CPFprojectname']);
     $caller = $_POST['CPFmyname'];
     $caller_id = $_SESSION['idhash'];
       
