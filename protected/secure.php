0649c22ff55ed29aa0cb2000272613b8b2f3561a
Richard Bull <richard.allan.b@gmail.com>
2014-09-23 13:52:34 -0700

    Redacted sensitive information

--- protected/secure.php
+++ protected/secure.php
@@ -2,8 +2,8 @@
 // This file MUST stay outside of document root.
 // If it is ever found in document root I will smack you.
 define ('mysqli_host', '104.131.195.24');
-define ('mysqli_user',  'niner');
-define ('mysqli_pw', 'MongooseHippopotomoose');
+define ('mysqli_user',  '-');
+define ('mysqli_pw', '-');
 define ('mysqli_port', '3306');
-define ('mysqli_db', 'teamNine');
+define ('mysqli_db', '-');
 ?>
